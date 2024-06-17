a45fd8113f72ab0bfde5c00e66240d49f0cde960
Αχιλλέας <47496934+AchillesKastanas@users.noreply.github.com>
2024-06-17 13:17:12 +0300

    created RoomManager

--- scripts/DungeonGenerator.gd
+++ scripts/DungeonGenerator.gd
@@ -11,20 +11,18 @@ var map_dimensions = Vector2( map_size, map_size )
 # Room size setup
 var room_dimensions = Vector2( room_size, room_size )
 
-# Tilemap referece
-@onready var tile_map = $"../../TileMap"
-
 
 func _ready():
-	var generated_map_rooms = []
-	generated_map_rooms = generate_map()
-	draw_map( generated_map_rooms )
+	# Room manager reference
+	var room_manager = get_node( "RoomGenerator" )
 
-# Block coordinates to actual World coordinates convertion
-func block_to_world_coords( block_position: Vector2 ) -> Vector2:
-	return block_position
-	#return block_position * block_size
-	
+	# Start the generation
+	if room_manager:
+		var generated_map_rooms = generate_map()
+		draw_map(generated_map_rooms, room_manager)
+	else:
+		print( "RoomManager node not found or not ready yet ? " )
+
 # Room coordinates to actual World coordinates convertion
 func room_to_world_coords( room_position: Vector2 ) -> Vector2:
 	return room_position * room_size
@@ -37,7 +35,7 @@ func is_vector2_in_vector2array( vector2, vector2array ):
 	return false
 
 # Draw map
-func draw_map( generated_map_rooms: Array[Vector2] ):
+func draw_map( generated_map_rooms: Array[Vector2], room_manager ):
 	for room_pos_x in range( map_dimensions.x ):
 		for room_pos_y in range( map_dimensions.y ):
 			var is_room_empty = true
@@ -52,93 +50,7 @@ func draw_map( generated_map_rooms: Array[Vector2] ):
 			# Pick a random entrance point for each room
 			var room_entrance_coords = Vector2( room_starting_coords.x + 2, room_starting_coords.y )
 			# Check if 
-			draw_room( room_starting_coords, room_ending_coords, room_entrance_coords, is_room_empty )
-
-# Draw Room - Transfer each room into a different scene ? Avoid all the world positioning ?
-func draw_room( room_starting_coords: Vector2, room_ending_coords: Vector2, entrance_coords: Vector2, is_room_empty: bool ):
-	for room_pos_x in range( room_starting_coords.x, room_ending_coords.x ):
-		for room_pos_y in range( room_starting_coords.y, room_ending_coords.y ):
-			# Block position relative to room
-			var block_pos = Vector2( room_pos_x, room_pos_y )
-			# Block position relative to the world
-			var actual_block_pos = block_to_world_coords( block_pos )
-			# Wall -> Is on the outer part of the room
-			var is_wall = false
-			var is_door = false
-			if room_pos_x == room_starting_coords.x or room_pos_x == ( room_starting_coords.x + room_dimensions.x - 1 ) or room_pos_y == room_starting_coords.y or room_pos_y == ( room_starting_coords.y + room_dimensions.y - 1 ):
-				is_wall = true
-			# TODO - USE ACTUALL WORLD COORDS
-			if block_pos == entrance_coords:
-				is_door = true
-			draw_block( actual_block_pos, is_wall, is_door, is_room_empty )
-
-# Draw Cell in given position
-#func draw_block( block_pos, is_wall, is_door, is_room_empty ):
-	#var block_label = Label.new()
-	#if is_door:
-		#block_label.text = " "
-	## Is on the outer part of the room
-	#elif is_wall:
-		#block_label.text = "#"
-	## Is on the inner part of the room
-	#else:
-		#block_label.text = "."
-		#if is_room_empty:
-			#block_label.add_theme_color_override( "font_color", Color( "333333" ) )
-		#else:
-			#block_label.add_theme_color_override( "font_color", Color( "FFFDD0" ) )
-	#block_label.add_theme_font_size_override( "font_size", block_size )
-	## Place block in the world
-	#block_label.position = block_pos
-	#add_child( block_label )
-
-func draw_block(block_pos: Vector2, is_wall: bool, is_door: bool, is_room_empty: bool):
-	var tile_id = -1
-	
-	if not is_room_empty:
-		if is_door:
-			tile_id = find_tile_id( Vector2i( 10, 1 ) )   # Atlas coordinates for doors
-		elif is_wall:
-			tile_id = find_tile_id( Vector2i( 0, 2 ) )   # Atlas coordinates for walls
-		else:
-			tile_id = find_tile_id( Vector2i( 8, 5 ) )   # Atlas coordinates for floors
-	else:
-		var random_value = randi() % 100 # 0 - 100
-		const void_spawn_chance = 90
-		
-		if random_value <= void_spawn_chance:
-			# 80% change
-			tile_id = find_tile_id( Vector2i( 16, 0 ) )   # Atlas coordinates for empty rooms / void
-		else:
-			# 20% chance
-			tile_id = find_tile_id( Vector2i( 2, 34 ) )   # Atlas coordinates for empty rooms / trees
-	
-	if tile_id != Vector2i( -1, -1 ):
-		tile_map.set_cell( 0, block_pos, 0, tile_id )
-
-	# Customize tile appearance based on additional conditions
-	#if is_room_empty:
-		# Adjust tile appearance (e.g., color)
-		#tile_map.set_cell_color(block_pos, Color(0.2, 0.2, 0.2))  # Example: Set tile color to dark gray
-	#else:
-		# Adjust tile appearance differently (e.g., color)
-		#tile_map.set_cell_color(block_pos, Color(1, 1, 0.82))  # Example: Set tile color to light yellow
-
-func find_tile_id( tile_id: Vector2i ) -> Vector2i:
-	var tileset_source = tile_map.tile_set.get_source(0)  # Assuming tile_map and its tile_set are already defined
-	
-	var tileset_tiles_count = tileset_source.get_tiles_count()
-	
-	for i in range( tileset_tiles_count ):
-		var current_tile_id = tileset_source.get_tile_id( i )
-		
-		if current_tile_id == tile_id:
-			return tile_id
-	
-	return Vector2i( -1, -1 )  # Tile not found
-
-
-# ------------------------------------------ TESTING GROUNDS 
+			room_manager.draw_room( room_starting_coords, room_ending_coords, room_entrance_coords, is_room_empty )
 
 func generate_map() -> Array[Vector2]:
 	var created_rooms: Array[Vector2] = []
---
+++ scripts/RoomGenerator.gd
@@ -0,0 +1,71 @@
+extends Node2D
+
+
+# Block size setup
+const map_size = 5    # rooms
+const room_size = 16  # blocks
+const block_size = 12 # pixels ? ( Currently is the Label size )
+# Map size setup
+var map_dimensions = Vector2( map_size, map_size )
+# Room size setup
+var room_dimensions = Vector2( room_size, room_size )
+
+@onready var tile_map = $"../../TileMap"
+
+func _ready():
+	print( "Room Manager Ready" )
+	
+# Loads the requested tile from the tilemap based on Atlas coordinates ( tile_id )
+func find_tile_id( tile_id: Vector2i ) -> Vector2i:
+	var tileset_source = tile_map.tile_set.get_source(0)  # Assuming tile_map and its tile_set are already defined
+	
+	var tileset_tiles_count = tileset_source.get_tiles_count()
+	
+	for i in range( tileset_tiles_count ):
+		var current_tile_id = tileset_source.get_tile_id( i )
+		
+		if current_tile_id == tile_id:
+			return tile_id
+	
+	return Vector2i( -1, -1 )  # Tile not found
+
+# Draw Room
+func draw_room( room_starting_coords: Vector2, room_ending_coords: Vector2, entrance_coords: Vector2, is_room_empty: bool ):
+	for room_pos_x in range( room_starting_coords.x, room_ending_coords.x ):
+		for room_pos_y in range( room_starting_coords.y, room_ending_coords.y ):
+			# Block position relative to room
+			var block_pos = Vector2( room_pos_x, room_pos_y )
+			# Wall -> Is on the outer part of the room
+			var is_wall = false
+			var is_door = false
+			if room_pos_x == room_starting_coords.x or room_pos_x == ( room_starting_coords.x + room_dimensions.x - 1 ) or room_pos_y == room_starting_coords.y or room_pos_y == ( room_starting_coords.y + room_dimensions.y - 1 ):
+				is_wall = true
+			# TODO - USE ACTUALL WORLD COORDS
+			if block_pos == entrance_coords:
+				is_door = true
+			draw_block( block_pos, is_wall, is_door, is_room_empty )
+
+func draw_block( block_pos: Vector2, is_wall: bool, is_door: bool, is_room_empty: bool ):
+	var tile_id = -1
+	
+	if not is_room_empty:
+		if is_door:
+			tile_id = find_tile_id( Vector2i( 10, 1 ) )   # Atlas coordinates for doors
+		elif is_wall:
+			tile_id = find_tile_id( Vector2i( 0, 2 ) )   # Atlas coordinates for walls
+		else:
+			tile_id = find_tile_id( Vector2i( 8, 5 ) )   # Atlas coordinates for floors
+	else:
+		var random_value = randi() % 100 # 0 - 100
+		const void_spawn_chance = 90
+		
+		if random_value <= void_spawn_chance:
+			# 80% change
+			tile_id = find_tile_id( Vector2i( 16, 0 ) )   # Atlas coordinates for empty rooms / void
+		else:
+			# 20% chance
+			tile_id = find_tile_id( Vector2i( 2, 34 ) )   # Atlas coordinates for empty rooms / trees
+	
+	if tile_id != Vector2i( -1, -1 ):
+		tile_map.set_cell( 0, block_pos, 0, tile_id )
+
